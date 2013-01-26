37a5aed0b30f1a81fbd424709a4f3541b24e1d94
Alex Gilliland-Lloyd <algl3@yahoo.com>
2013-01-26 02:16:31 -0600

    MainMenu Working?
    Can't quit. Not sure why.
    
    Can select a virus by name. Starts the game.

--- Assets/MainMenu/Scripts/GuiScript.cs
+++ Assets/MainMenu/Scripts/GuiScript.cs
@@ -15,18 +15,59 @@ public class GuiScript : MonoBehaviour {
 	}
 	
 	void OnGUI(){
+		if(menuNumber == 1){
+			menuCharacterSelect();			
+		} else {
+			menuMain();
+		}
+			
+	}
+	
+	void menuCharacterSelect(){
+		// Make a background box
+		GUI.Box(new Rect(10,10,100,180), "Loader Menu");
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,40,80,20), "Adam")) {
+			//	Select character and change to the right level
+			Application.LoadLevel("TestLevel");
+		}
+
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,80,20), "Buddy")) {
+			Application.LoadLevel("TestLevel");
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(20,100,80,20), "Charlie")) {
+			Application.LoadLevel("TestLevel");
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(20,130,80,20), "Meth Addict")) {
+			Application.LoadLevel("TestLevel");
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(20,160,80,20), "Cancel")) {
+			menuNumber = 0;
+		}
+	}
+	
+	
+	void menuMain() {
 		// Make a background box
 		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
 
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
-			Application.LoadLevel(1);
+		if(GUI.Button(new Rect(20,40,80,20), "Start")) {
+			menuNumber = 1;
 		}
 
 		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Level 2")) {
-			Application.LoadLevel(2);
-		}	
+		if(GUI.Button(new Rect(20,70,80,20), "Quit")) {
+			Application.Quit();
+		}
 	}
 	
 	// Update is called once per frame
