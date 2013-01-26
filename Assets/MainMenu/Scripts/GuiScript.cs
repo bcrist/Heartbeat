bbc218436c4bf15781810658ddce5604a3fabd94
Alex Gilliland-Lloyd <algl3@yahoo.com>
2013-01-26 02:08:11 -0600

    MainMenu Framework
    A menu that allows you start level 1 or level 2.
    
    We just need a level 1 and 2.

---
+++ Assets/MainMenu/Scripts/GuiScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class GuiScript : MonoBehaviour {
+	
+	/*
+	 * 0 = Main Menu
+	 * 1 = Character Select
+	 */
+	public int menuNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnGUI(){
+		// Make a background box
+		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
+			Application.LoadLevel(1);
+		}
+
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,80,20), "Level 2")) {
+			Application.LoadLevel(2);
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
