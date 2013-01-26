313d3853e9cbb39bcc3be4bfccdfd35d9ce7fe55
Ben Crist <ben@magicmoremagic.com>
2013-01-26 15:04:53 -0600

    Flow objects 2

--- Assets/Scripts/FlowObject.cs
+++ Assets/Scripts/FlowObject.cs
@@ -3,25 +3,34 @@ using System.Collections;
 
 public class FlowObject : MonoBehaviour {
 	
-	public Vector3 flow_direction;
+	public Vector3 flow_vector;
+	public float pulse_interval;
 	
 	private ArrayList objects_in_flow;
 	private float last_flow_time;
 	
-	void OnCollisionEnter (Collision collision)
+	void Start()
 	{
-		if (collision.gameObject.tag == "Player")
-			objects_in_flow.Add(collision.gameObject);
+		objects_in_flow = new ArrayList();
+		last_flow_time = 0;
 	}
 	
-	void onCollisionExit(Collision collision)
+	void OnTriggerEnter (Collider col)
 	{
-		objects_in_flow.Remove(collision.gameObject);
+		if (col.tag == "Player")
+		{
+			objects_in_flow.Add(col.gameObject);
+		}
+	}
+	
+	void OnTriggerExit(Collider col)
+	{
+		objects_in_flow.Remove(col.gameObject);
 	}
 	
 	
 	void FixedUpdate () {
-		if (Time.fixedTime - last_flow_time > 4f)
+		if (Time.fixedTime - last_flow_time > pulse_interval)
 		{
 			last_flow_time = Time.fixedTime;
 			pushObjects();
@@ -32,11 +41,9 @@ public class FlowObject : MonoBehaviour {
 	{
 		for (int i = 0; i < objects_in_flow.Count; ++i)
 		{
-			GameObject obj = (GameObject)objects_in_flow[i];
-			
+			GameObject obj = objects_in_flow[i] as GameObject;
 			Rigidbody rb = obj.GetComponent<Rigidbody>();
-			
-			rb.AddForce (flow_direction);
+			rb.AddForce (flow_vector);
 		}
 	}
 }
