e27f72257d93addd2e5f297ce0af3d02cfec7796
loud alex <alexander_paul_seidlinger@hotmail.com>
2013-01-26 12:03:24 -0600

    i'm a hooligan

---
+++ Assets/LoudAlex/ContinueGameGUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class ContinueGameGUI : MonoBehaviour {
+	
+	public int menuNumber = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnGUI()
+	{
+		// Make a background box
+		GUI.Box(new Rect(10,10,120,90), "Continue Menu");
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,40,100,20), "Continue")) {
+			//leveltag will be set equal to that actual level tag as a string
+			Application.LoadLevel(Application.loadedLevel);
+		}
+
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,100,20), "Level Select")) {
+			menuNumber = 2;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
